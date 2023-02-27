07f1e01481bd5807e16a824548cee0713bade86d
Tomas13d <tomas.demobio@gmail.com>
2023-02-27 18:54:45 -0300

    feat: update

--- pages/_app.js
+++ pages/_app.js
@@ -2,30 +2,8 @@ import "../styles/globals.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import Nabvar from "../components/navbar.jsx";
-import { useEffect, useState } from "react";
 
 export default function App({ Component, pageProps }) {
-const [api, setApi] = useState([])
-
-  useEffect(()  => {
-
-    const excelRequest = async () => {
-      try{
-        const response = await (await fetch("/api/test")).json()
-        setApi(response)
-      } catch(err) {  
-        console.log("err-->",err);
-      }
-    }
-  
-    excelRequest()
- 
-}, [])
-
-console.log("api -->", api);
-
-
-
   return (
     <>
       <Nabvar />
