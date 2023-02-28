9eeba4ef654402b7b24f6ce784ab200edf92e5ce
Tomas13d <tomas.demobio@gmail.com>
2023-02-28 09:49:53 -0300

    feat: our courses section

--- sections/ourCourses.jsx
+++ sections/ourCourses.jsx
@@ -1,4 +1,7 @@
 import Header from "../components/header";
+import CursoIntegral_illustation from "../components/illustrations/CursoIntegral_illustation";
+import CursoAvanzado_illustation from "../components/illustrations/CursoAvanzado_illustation";
+import CursoCripto_illustration from "../components/illustrations/CursoCripto_illustration";
 import styles from "../styles/sectionsStyles/ourCourses.module.css";
 
 export default function OurCourses({ title, subtitle, courses }) {
@@ -8,9 +11,10 @@ export default function OurCourses({ title, subtitle, courses }) {
     >
       <Header title={title} subtitle={subtitle} />
       <div className={styles.timeline}>
+
         <div className={`${styles.individualCourseCont} ${styles.left}`}>
           <div className={styles.content}>
-          <h6 className="small-headers white-blue-text">Cursos</h6>
+            <h6 className="small-headers white-blue-text">Cursos</h6>
             <p className="general-text white-blue-text">
               Tendrás la oportunidad de adquirir los conocimientos necesarios
               para convertirte en un inversor independiente y competente en el
@@ -20,19 +24,46 @@ export default function OurCourses({ title, subtitle, courses }) {
             </p>
             <button className="btn-sky">Saber Más</button>
           </div>
+          <div className={` ${styles.ilustrationRight}`}>
+            <CursoIntegral_illustation customClass={styles.courseIllustrarionSize}/>
+          </div>
         </div>
+
         <div className={`${styles.individualCourseCont} ${styles.right}`}>
           <div className={styles.content}>
-            <h2>2016</h2>
-            <p>Lorem ipsum..</p>
+            <h6 className="small-headers white-blue-text">Cursos</h6>
+            <p className="general-text white-blue-text">
+              Tendrás la oportunidad de adquirir los conocimientos necesarios
+              para convertirte en un inversor independiente y competente en el
+              mundo de las criptomonedas. Aprenderás a hacer tus propias
+              inversiones sin necesidad de arriesgar tu capital en manos de
+              terceros, y tendrás la disponibilidad permanente de tus fondos.
+            </p>
+            <button className="btn-sky">Saber Más</button>
+          </div>
+          <div className={`${styles.ilustrationLeft}`}>
+            <CursoAvanzado_illustation customClass={styles.courseIllustrarionSize}/>
           </div>
         </div>
+
         <div className={`${styles.individualCourseCont} ${styles.left}`}>
           <div className={styles.content}>
-            <h2>2017</h2>
-            <p>Lorem ipsum..</p>
+            <h6 className="small-headers white-blue-text">Cursos</h6>
+            <p className="general-text white-blue-text">
+              Tendrás la oportunidad de adquirir los conocimientos necesarios
+              para convertirte en un inversor independiente y competente en el
+              mundo de las criptomonedas. Aprenderás a hacer tus propias
+              inversiones sin necesidad de arriesgar tu capital en manos de
+              terceros, y tendrás la disponibilidad permanente de tus fondos.
+            </p>
+            <button className="btn-sky">Saber Más</button>
+          </div>
+          <div className={` ${styles.ilustrationRight}`}>
+            <CursoCripto_illustration customClass={styles.courseIllustrarionSize}/>
           </div>
         </div>
+
+
       </div>
     </section>
   );
