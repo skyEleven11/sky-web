d7c2f6cb0690c06c9b0eb528f7f70d571c0b8e1a
Tomas13d <tomas.demobio@gmail.com>
2023-03-02 15:15:32 -0300

    feat: our courses link

--- sections/ourCourses.jsx
+++ sections/ourCourses.jsx
@@ -1,55 +1,73 @@
 import dynamic from "next/dynamic";
+import Link from "next/link";
 import { useEffect, useState } from "react";
 import Header from "../components/header";
 import CursoIntegral_illustation from "../components/illustrations/CursoIntegral_illustation";
-;
 import styles from "../styles/sectionsStyles/ourCourses.module.css";
 
-
 export default function OurCourses({ title, subtitle, courses }) {
-  const [illustration, setIllustration] = useState()
-  
-  useEffect(()=>{
-    if(courses[1]){
-      const illArray = []
-      courses.map((illustration)=> {
-        const DynamicIllustration =  dynamic(() => import(`../components/illustrations/${illustration.ILUSTRACION}`,{ssr: false}))
-        illArray.push(<DynamicIllustration customClass={styles.courseIllustrarionSize}/>)
-      })
-      setIllustration(illArray)
-    }
-  },[courses])
+  const [illustration, setIllustration] = useState();
 
-  return courses[1] && (
-    <section
-      className={`row d-flex justify-content-between align-items-center ${styles.ourCoursesCont}`}
-    >
-      <Header title={title} subtitle={subtitle} color="darkBlue" />
-      <div className={styles.timeline}>
-        {courses &&
-          courses[0] &&
-          courses.map((course, i) => (
-            <>
-              <div className={`${styles.individualCourseCont} ${ i%2 === 0 ? styles.left : `${styles.right} flex-row-reverse`}`} key={i + (new Date).getTime()}>
-                <div className={styles.content}>
-                  <h6 className="small-headers white-blue-text">{course.NOMBRE}</h6>
-                  <p className="general-text white-blue-text">
+  useEffect(() => {
+    if (courses[1]) {
+      const illArray = [];
+      courses.map((illustration) => {
+        const DynamicIllustration = dynamic(() =>
+          import(`../components/illustrations/${illustration.ILUSTRACION}`, {
+            ssr: false,
+          })
+        );
+        illArray.push(
+          <DynamicIllustration customClass={styles.courseIllustrarionSize} />
+        );
+      });
+      setIllustration(illArray);
+    }
+  }, [courses]);
+
+  return (
+    courses[1] && (
+      <section
+        className={`row d-flex justify-content-between align-items-center ${styles.ourCoursesCont}`}
+      >
+        <Header title={title} subtitle={subtitle} color="darkBlue" />
+        <div className={styles.timeline}>
+          {courses &&
+            courses[0] &&
+            courses.map((course, i) => (
+              <>
+                <div
+                  className={`${styles.individualCourseCont} ${
+                    i % 2 === 0
+                      ? styles.left
+                      : `${styles.right} flex-row-reverse`
+                  }`}
+                  key={i + new Date().getTime()}
+                >
+                  <div className={styles.content}>
+                    <h6 className="small-headers white-blue-text">
+                      {course.NOMBRE}
+                    </h6>
+                    <p className="general-text white-blue-text">
                       {course.DEFINICION_BREVE}
-                  </p>
-                  <button className="btn-sky">Saber Más</button>
+                    </p>
+                    <Link href={course.SABER_MAS}>
+                      <button className="btn-sky">Saber Más</button>
+                    </Link>
+                  </div>
+                  {illustration && illustration[i]}
                 </div>
-                {illustration && illustration[i]}
-              </div>
-            </>
-          ))}
-      </div>
-      <div className={`${styles.bubble} ${styles.bubbleRight}`}>
-        <p className={styles.message}>
-          Excelente servicio. Lo más importante es que constantemente se van
-          perfeccionando y tienen un seguimiento personalizado hacia cada
-          individuo, generando que uno continúe estudiando y aprendiendo.
-        </p>
-      </div>
-    </section>
+              </>
+            ))}
+        </div>
+        <div className={`${styles.bubble} ${styles.bubbleRight}`}>
+          <p className={styles.message}>
+            Excelente servicio. Lo más importante es que constantemente se van
+            perfeccionando y tienen un seguimiento personalizado hacia cada
+            individuo, generando que uno continúe estudiando y aprendiendo.
+          </p>
+        </div>
+      </section>
+    )
   );
 }
