c34972e00298161d20ef9138a7aef1c417e78ad6
Tomas13d <tomas.demobio@gmail.com>
2023-03-03 15:07:41 -0300

    wip: individual view Courses

--- pages/cursos/[indCourse].jsx
+++ pages/cursos/[indCourse].jsx
@@ -38,22 +38,39 @@ export default function IndividualViewService() {
         <div
           className={`row d-flex justify-content-between align-items-center ${styles.individualCourseCont}`}
         >
-          
-
           <div className={`col-6 ${styles.topMarginServ} ${styles.blueCircle}`}>
             {courseIllustration}
           </div>
           <div className={`col-6 ${styles.topMarginServ}`}>
             <p className="general-text dark-blue-text">
-              Emision por solicitud de clientes de certificado de ingresos.
-              Documentación legal con certificación y legalización en consejo
-              Profesional de ciencias Económicas de la Provincia de San Luis.
+              ¡Bienvenido al curso integral de trading! En este curso, te
+              ofrecemos la oportunidad de adquirir los conocimientos necesarios
+              para convertirte en un trader independiente y eficiente.
+              Aprenderás a hacer tus propias inversiones sin necesidad de
+              arriesgar tu capital en manos de terceros, y tendrás la
+              disponibilidad permanente de tus fondos. Nuestro objetivo es
+              educar a nuestros clientes para que sean capaces de movilizar su
+              capital de manera independiente y gestionar el riesgo asociado al
+              trading de manera efectiva. Con nuestro enfoque integral,
+              aprenderás a generar utilidades de manera constante a través de la
+              aplicación de técnicas avanzadas de trading y una gestión
+              eficiente del riesgo. Con nuestro equipo de expertos, tendrás
+              acceso a una amplia variedad de recursos y herramientas de trading
+              que te ayudarán a perfeccionar tus habilidades y a maximizar tus
+              ganancias. Así que si estás buscando la oportunidad de convertirte
+              en un trader profesional y obtener ingresos consistentes en el
+              mercado financiero, ¡este curso es para vos!
             </p>
+            <Link href={"#"}>
+              <button className="btn-sky">Adquirir Curso</button>
+            </Link>
           </div>
-          <div className={`col-5`}>
+          <div className={`col-5 ${styles.topMarginCouse}`}>
             <Accordion defaultActiveKey="0">
               <Accordion.Item eventKey="0">
-                <Accordion.Header className={styles.accordionHeaders}>Accordion Item #1</Accordion.Header>
+                <Accordion.Header className={styles.accordionHeaders}>
+                  DESCRIPCION{" "}
+                </Accordion.Header>
                 <Accordion.Body>
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
                   do eiusmod tempor incididunt ut labore et dolore magna aliqua.
@@ -80,16 +97,7 @@ export default function IndividualViewService() {
               </Accordion.Item>
             </Accordion>
           </div>
-          <div className={`col-6`}>
-            <p className="general-text dark-blue-text">
-              Si estás buscando la oportunidad de convertirte en un trader
-              profesional y obtener ingresos consistentes en el mercado
-              financiero ¡este curso es para ti!
-            </p>
-            <Link href={"#"}>
-              <button className="btn-sky">Adquirir Curso</button>
-            </Link>
-          </div>
+         
         </div>
       </section>
     </Container>
